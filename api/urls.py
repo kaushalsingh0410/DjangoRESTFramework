2770133268fa8efffcc57aa249e6798966357469
kaushalsingh0410 <kaushalsingh@gmail.com>
2025-06-07 09:27:50 +0530

    Employee done with ModelViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('employee',views.EmployeeViewset)
 
 urlpatterns = [
     path('studend/',views.StudentView,name = 'StudentView' ),
     path('studend/<int:pk>/',views.studentAction,name = 'studentAction' ),
     
-    path('employee/',views.EmployeeList.as_view(),name = 'employeeList' ),
-    path('employee/<int:pk>',views.EmployeeACtions.as_view(),name = 'employeeACtions' ),
-
-
+    # path('employee/',views.EmployeeList.as_view(),name = 'employeeList' ),
+    # path('employee/<int:pk>',views.EmployeeACtions.as_view(),name = 'employeeACtions' ),
+ 
+    path('',include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.views import APIView 
 from django.http import Http404
 from rest_framework import generics,mixins
-
+from rest_framework.viewsets import ModelViewSet
 
 
 @api_view(['GET','POST'])
@@ -83,6 +83,37 @@ def studentAction(request,pk):
         
 
 
-class EmployeeList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+# class EmployeeList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+#     def get(self,request):
+#         return self.list(request)
+    
+#     def post(self,request):
+#         return self.create(request)
+    
+# class EmployeeACtions(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+#     def get(self,request,pk):
+#         return self.retrieve(request,pk)
+        
+#     def put(self,request,pk):
+#         return self.update(request,pk)
+        
+#     def delete(self,request,pk):
+#         return self.destroy(request,pk)
+        
+
+
+# class EmployeeList(generics.ListCreateAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+# class EmployeeACtions(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class  = EmployeeSerializer
+#     lookup_field = 'pk'
+
+class EmployeeViewset(ModelViewSet):
     queryset = Employee.objects.all()
-     
+    serializer_class = EmployeeSerializer
